b7d28e39353a2db10efbe735b9f2786f252f1451
Erica Yee <23279506+ericayee@users.noreply.github.com>
2020-02-10 17:00:08 -0500

    working todo list with localstorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,156 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+
+  componentDidMount() {
+    this.hydrateStateWithLocalStorage();
+
+    // add event listener to save state to localStorage
+    // when user leaves/refreshes page
+    window.addEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+
+    // saves if component has chance to unmount
+    this.saveStaetToLocalStorage();
+  }
+
+  updateInput(key, value) {
+    // update react state
+    this.setState({ [key]: value });
+  }
+
+  addItem() {
+    // create a new item
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    // copy current list of items
+    const list = [...this.state.list];
+
+    // add the new item to the list
+    list.push(newItem);
+
+    // update state with new list, reset the new item input
+    this.setState({
+      list,
+      newItem: ""
+    });
+
+    // update localStorage
+    localStorage.setItem("list", JSON.stringify(list));
+    localStorage.setItem("newItem", "")
+  }
+
+  deleteItem(id) {
+    // copy current list of items
+    const list = [...this.state.list];
+    // filter out the item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({ list: updatedList });
+
+    // update LocalStorage
+    localStorage.setItem("list", JSON.stringify(updatedList));
+  }
+
+  hydrateStateWithLocalStorage() {
+    // for all items in state
+    for (let key in this.state) {
+      // if key exists in localStorage
+      if (localStorage.hasOwnProperty(key)) {
+        // get key's value from localStorage
+        let value = localStorage.getItem(key);
+
+        // parse localStorage string and setState
+        try {
+          value = JSON.parse(value);
+          this.setState({
+            [key]: value
+          });
+        } catch (e) {
+          // handle empty string
+          this.setState({
+            [key]: value
+          });
+        }
+      }
+    }
+  }
+
+  saveStateToLocalStorage() {
+    // for every item in React state
+    for (let key in this.state) {
+      // save to localStorage
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">IS 4300: I5 Gui Implementation</h1>
+          <h3>By Erica Yee</h3>
+        </header>
+        <div
+          style={{
+            padding: 50,
+            textAlign: "left",
+            maxWidth: 500,
+            margin: "auto"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Add an item to the list
+          <br />
+          <input
+            type="text"
+            placeholder="Type item here"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <button
+            onClick={() => this.addItem()}
+            disabled={!this.state.newItem.length}
+          >
+            &#43; Add
+          </button>
+          <br /> <br />
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button onClick={() => this.deleteItem(item.id)}>
+                    Remove
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
